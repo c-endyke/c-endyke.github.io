09cfdb7cea2af9e0eea9b0ab4e9f3544c980cf03
Caitlin Endyke <endyke@umich.edu>
2018-04-05 19:19:09 -0400

    Portfolio Updates

---
+++ 539/caitlin-endyke/js/main.js
@@ -0,0 +1,17 @@
+    $(document).ready(function(){
+    $("#waterfront-title").hide().slideDown(2000);
+    });
+
+
+    $(document).ready(function(){
+    $("#home-about").hide().fadeIn(2000);
+    });
+
+
+$(document).on('click', 'a[href^="#"]', function (event) {
+    event.preventDefault();
+
+    $('html, body').animate({
+        scrollTop: $($.attr(this, 'href')).offset().top
+    }, 500);
+});
